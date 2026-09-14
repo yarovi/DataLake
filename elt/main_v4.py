db7f25694d149a6cd08fb546ddd5872a905555aa
Yasmani Rosado <tas185@gmail.com>
2026-09-14 18:46:00 -0500

    add upload raw elt

---
+++ elt/main_v4.py
@@ -0,0 +1,19 @@
+from elt.extract_raw import extract_table
+from elt.load_bronze_v4 import load_to_bronze
+
+
+def main():
+    orders = extract_table("orders")
+
+    print(
+        f"[ELT-EXTRACT] Registros orders: {len(orders)}"
+    )
+
+    load_to_bronze(
+        dataframe=orders,
+        table_name="orders"
+    )
+
+
+if __name__ == "__main__":
+    main()
